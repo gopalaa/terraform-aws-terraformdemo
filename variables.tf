ba85646db9c5ba904b7a54bcc3404f103eb66d53
Cheran Vyshu <cheranvyshu@Cherans-MacBook-Pro.local>
2021-03-31 20:15:25 -0700

    Terraform Demo

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "amiID" {
+  default = "ami-08962a4068733a2b6"
+}
+
+variable "keyname" {
+	default="myrehatkey" 
+}
+
+variable "vpcID" {
+	default="vpc-4cbe3c27"
+}
+
